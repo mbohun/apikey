8e494eb1a570fb7ab40a0dec173d9dfd2ddf91e7
mbohun <martin.bohun@gmail.com>
2014-10-09 14:52:07 +1100

    trying release-plugin (in the legacy, pre grails 2.3 mode) instead of the old publisher-plugin; intentionally did NOT regenerate the secure/encrypted env var-s - to trigger/examine the error message on deployment

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -46,6 +46,9 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
+        build ':release:2.2.1', ':rest-client-builder:1.0.3', {
+	    export = false
+        }
         runtime ":hibernate:$grailsVersion"
         runtime ":jquery:1.8.3"
         runtime ":resources:1.1.6"
